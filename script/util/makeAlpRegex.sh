abb35454f6e46abfd21542d71dba0c65b68e20f1
Hayato Mitomi <mitomihayato@mitomihayatonoMacBook-Pro.local>
2023-11-28 00:27:27 +0900

    consider echo.Group method

--- script/util/makeAlpRegex.sh
+++ script/util/makeAlpRegex.sh
@@ -7,9 +7,13 @@ if [ ! -e $FILE ]; then
   exit 1
 fi
 
-REGEX=""
+SED_ARGS=`egrep '([a-zA-Z0-9_]+\.Group\(|echo\.New\(\))' $FILE | sed -e 's/echo\.New()/echo\.Group\(\"\"\)/g' -e 's/^\t*\([a-zA-Z0-9]*\) := [a-zA-Z0-9_]*\.Group("\(.*\)".*$/s\/"\1"\/\2\/g/g'`
 
-for e in `cat $FILE | egrep 'e\.(GET|POST|PATCH|PUT|DELETE)\("/.*"' | sed 's/^.*"\//\//g' | sed 's/".*$//g' | sed 's/:[A-Za-z0-9_]*/\[\^\/]\+/g' | sort -r | uniq`; do
+REGEX=""
+for e in `egrep '[a-zA-Z0-9_]+\.(GET|POST|PATCH|PUT|DELETE)\("/.*"' $FILE | sed -e 's/^\t*//g' -e 's/^\([a-zA-Z0-9_]*\).*("\([a-zA-Z0-9_/:]*\)".*$/"\1"\2/g' | sort -r | uniq`; do
+  for arg in $SED_ARGS; do
+    e=`echo $e | sed -e $arg -e 's/:[a-zA-Z0-9_]*/[^\/]+/g'`
+  done
   REGEX=$REGEX,$e
 done
 
